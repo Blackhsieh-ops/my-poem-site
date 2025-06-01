d745a2b622794c1e8a54b0a92f368592a6dfc4d6
Blackhsieh-ops <arsldavid0304@gmail.com>
2025-06-01 15:56:17 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// 修改 base 為你 GitHub Pages 網址尾端的 repo 名稱
 export default defineConfig({
+  base: '/my-poem-site/',
   plugins: [react()],
-  base: '/my-poem-site/', // 這裡填你的 GitHub Pages 專案名稱
 });
-
